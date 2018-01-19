52848b242ebfa7c18988e6171e54147549f7cc91
Renaud BERNARD <renaud.bernard@p59production.com>
2018-01-19 16:01:29 +0100

    Links and premaring jQuery
    Adding links of CSS and Js files + preparing work of jQuery function event.

--- js/main.js
+++ js/main.js
@@ -8,3 +8,37 @@ Bouton 4 : fait réaparaitre le rectangle
 Bouton 5 : augmente la hauteur de 10px, s'il dépasse 100px, il remet la hauteur à 10px
 
 */
+//On sélectionne le rectangle
+var rec = $('#rectangle');
+//On sélectionne les boutons
+var btn1 = $('bouton1'), btn2 =$('bouton2'), btn3 =$('bouton3'), btn4 =$('bouton4'), btn5 =$('bouton5')
+
+
+btn1.on('click', function() {
+//Color=Vert
+
+}
+btn2.on('click', function() {
+//Color=#356188
+	
+}
+btn3.on('click', function() {
+//Disparaître
+	
+}
+btn4.on('click', function() {
+//Apparaître
+	
+}
+btn5.on('click', function() {
+//+10px
+	
+}
+
+
+/*
+
+.show() //Apparaître
+.hide() //Disparaître
+.width ()
+*/
